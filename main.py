b410ed519139f6302034170f5297d4f44d27b8de
crowspirit <61355446+crowspirit@users.noreply.github.com>
2024-05-05 22:01:29 +0300

    Update main.py
    змінив аргумент у методі створення бота

--- main.py
+++ main.py
@@ -130,7 +130,7 @@ group_by_department = {
 }
 
 
-bot = telebot.TeleBot(config.TOKEN, timeout=10000)
+bot = telebot.TeleBot(config.TOKEN, apihelper_timeout=10000)
 bot.send_message(config.Admin, 'БОТ ЗАПУЩЕН')
 
 
